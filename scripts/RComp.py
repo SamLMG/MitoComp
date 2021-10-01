476cf084faaa987f394be4bdb511d3cdd1d7d8d2
Leeming Samuel <leeming@l46.vsc4.prov>
2021-10-01 10:47:54 +0200

    add level -output to file paths

--- scripts/RComp.py
+++ scripts/RComp.py
@@ -15,7 +15,7 @@ def rev_comp(seq):
             return(RCseq)
 
 
-with open("compare/RC_assemblies.txt", 'r') as RC_file:
+with open("output/compare/RC_assemblies.txt", 'r') as RC_file:
         for line in RC_file:
                 fasta = line.rstrip()
                 fasta_prefix = '.'.join(fasta.split(".")[0:5])
@@ -28,9 +28,9 @@ with open("compare/RC_assemblies.txt", 'r') as RC_file:
                                 out_file.close()
         #for fasta in RC_file:
 
-with open("compare/forward_assemblies.txt", 'r') as FA_file:
+with open("output/compare/forward_assemblies.txt", 'r') as FA_file:
 	for LINE in FA_file:
 		ffasta = LINE.rstrip()
 		dest_ffasta = ffasta.split("/")[2]
-		shutil.copy(ffasta, f"compare/alignment/clustalo/{dest_ffasta}")
+		shutil.copy(ffasta, f"output/compare/alignment/clustalo/{dest_ffasta}")
 FA_file.close()
