bbee57a8ed9c6cba9148cc87212c25bebce7b900
Leeming Samuel <leeming@n408-032.vsc4.prov>
2022-07-01 16:04:56 +0200

    Check ID in forward assemblies

--- scripts/RComp.py
+++ scripts/RComp.py
@@ -21,7 +21,7 @@ def rev_comp(seq):
 with open("output/stats/RC_assemblies.txt", 'r') as RC_file:
         for line in RC_file:
                 if sampleid not in line:
-                      print(sampleid, "was not found in line of assemblies. Will skip this line")
+                      print(sampleid, "was not found in line of RC assemblies. Will skip this line")
                       continue
                 fasta = line.rstrip()
                 fasta_prefix = '.'.join(fasta.split(".")[0:5])
@@ -34,8 +34,11 @@ with open("output/stats/RC_assemblies.txt", 'r') as RC_file:
         #for fasta in RC_file:
 
 with open("output/stats/forward_assemblies.txt", 'r') as FA_file:
-	for LINE in FA_file:
-		ffasta = LINE.rstrip()
+	for fline in FA_file:
+		if sampleid not in fline:
+                      print(sampleid, "was not found in line of forward assemblies. Will skip this line")
+                      continue
+		ffasta = fline.rstrip()
 		dest_ffasta = ffasta.split("/")[4]
 		x = ("output", ffasta.split("/")[1], "annotation/alignment/clustalo")
 		dest_dir = '/'.join(x)
